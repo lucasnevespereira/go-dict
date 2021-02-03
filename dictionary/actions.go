2ff1ca6b7508a4ae7ec86df658bb9ea073ae94ab
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-03 20:54:27 +0100

    list words of dictionary

--- dictionary/actions.go
+++ dictionary/actions.go
@@ -3,6 +3,7 @@ package dictionary
 import (
 	"bytes"
 	"encoding/gob"
+	"sort"
 	"strings"
 	"time"
 
@@ -27,5 +28,70 @@ func (d *Dictionary) Add(word string, definition string) error {
 	return d.db.Update(func(txn *badger.Txn) error {
 		return txn.Set([]byte(word), buffer.Bytes())
 	})
+}
+
+// Get functions reads entry from dictionary
+func (d *Dictionary) Get(word string) (Entry, error) {
+	var entry Entry
+
+	err := d.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(word))
+		if err != nil {
+			return err
+		}
+
+		entry, err = getEntry(item)
+		return err
+
+	})
+
+	return entry, err
+
+}
+
+// List returns all entries of the dictionary
+// []string is an alphabetical sorted array with the words
+// [string]entry is a map of the words and their definitions
+func (d *Dictionary) List() ([]string, map[string]Entry, error) {
+	entries := make(map[string]Entry)
+	err := d.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			entry, err := getEntry(item)
+			if err != nil {
+				return err
+			}
+			entries[entry.Word] = entry
+		}
+		return nil
+	})
+	return sortedKeys(entries), entries, err
+}
+
+func sortedKeys(entries map[string]Entry) []string {
+	keys := make([]string, len(entries))
+	for key := range entries {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	return keys
+}
+
+func getEntry(item *badger.Item) (Entry, error) {
+	var entry Entry
+	var buffer bytes.Buffer
+	err := item.Value(func(val []byte) error {
+		_, err := buffer.Write(val)
+		return err
+	})
+
+	dec := gob.NewDecoder(&buffer)
+	err = dec.Decode(&entry)
+	return entry, err
 
 }
--- main.go
+++ main.go
@@ -9,6 +9,12 @@ func main() {
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
+
+	d.Add("python", "An interpreted language")
+	words, entries, _ := d.List()
+	for _, word := range words {
+		fmt.Println(entries[word])
+	}
 }
 
 func handleErr(err error) {
