b0406c2a1ac520b94380f0254464ba1f086b331e
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-04 18:01:59 +0100

    added CLI to project

--- main.go
+++ main.go
@@ -1,17 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go-dict/dictionary"
 )
 
 func main() {
+	action := flag.String("action", "list", "Action to perform on the dictionary")
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
 
+	flag.Parse()
+	switch *action {
+	case "list":
+		actionList(d)
+	case "add":
+		actionAdd(d, flag.Args())
+	case "define":
+		actionDefine(d, flag.Args())
+	case "remove":
+		actionRemove(d, flag.Args())
+	default:
+		fmt.Printf("Unknown action: %v\n", *action)
+	}
 	d.Add("python", "An interpreted language")
 	words, entries, _ := d.List()
+	for _, word := range words {
+		fmt.Println(entries[word])
+	}
+}
+
+func actionAdd(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	def := args[1]
+
+	err := d.Add(word, def)
+	handleErr(err)
+	fmt.Printf("'%v' added to dictionary\n", word)
+}
+
+func actionDefine(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	entry, err := d.Get(word)
+	handleErr(err)
+	fmt.Println(entry)
+}
+
+func actionRemove(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	err := d.Remove(word)
+	handleErr(err)
+	fmt.Printf("word '%v' was removed", word)
+}
+
+func actionList(d *dictionary.Dictionary) {
+	words, entries, err := d.List()
+	handleErr(err)
 	for _, word := range words {
 		fmt.Println(entries[word])
 	}
